d59c18508e8484eb1c7fe6387c53d3af2d49d52f
Alperen Erdoğan <139046262+alperen27tr@users.noreply.github.com>
2024-01-04 16:42:25 +0300

    Add files via upload

---
+++ OOP_Homework/HastaneOtomasyon1/HastaneOtomasyon1/Doktor.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HastaneOtomasyon1
+{
+    internal class Doktor : HastaneCalisanlari
+    {
+        public int DiplomaNo { get; }
+        public string Brans { get; set; }
+
+        public Doktor(string ad, string soyad, string tc, string cinsiyet, int sicilNo, double ekGosterge, string departman, int diplomaNo, string brans)
+            : base(ad, soyad, tc, cinsiyet, sicilNo, ekGosterge, departman)
+        {
+            DiplomaNo = diplomaNo;
+            Brans = brans;
+        }
+
+        public override void BilgileriYazdir()
+        {
+            base.BilgileriYazdir();
+            Console.WriteLine($"Diploma No: {DiplomaNo}\nBranş: {Brans}");
+        }
+
+        public override void MaasHesapla()
+        {
+            base.MaasHesapla();
+        }
+    }
+}
